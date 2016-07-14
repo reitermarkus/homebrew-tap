edcd00d61cbeeeb7878c7d9d12b4b7b0d10d996a
Markus Reiter <me@reitermark.us>
2016-07-14 04:19:58 +0200

    Add Travis scripts.

--- Rakefile
+++ Rakefile
@@ -1,13 +1,8 @@
 require "rake/testtask"
-require "rubocop/rake_task"
 
 Rake::TestTask.new do |t|
   t.pattern = "test/**/*_test.rb"
   t.libs << "test"
 end
 
-RuboCop::RakeTask.new(:rubocop) do |t|
-  t.options = ["--force-exclusion"]
-end
-
-task default: [:test, :rubocop]
+task default: :test
---
+++ ci/travis/script.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+#
+# script.sh
+#
+# This file is meant to be sourced during the `script` phase of the Travis
+# build. Do not attempt to source or run it locally.
+#
+# shellcheck disable=SC1090
+. "${TRAVIS_BUILD_DIR}/ci/travis/helpers.sh"
+
+header 'Running script.sh...'
+
+if any_casks_modified; then
+  modified_casks=($(modified_cask_files))
+  run brew cask style "${modified_casks[@]}"
+fi
+
+if must_run_tests; then
+  run bundle exec rubocop
+  run bundle exec rake test
+fi
