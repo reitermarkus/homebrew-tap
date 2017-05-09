f81829390ee641bb6b50000def3a7017ef6c4f3b
Markus Reiter <me@reitermark.us>
2017-05-09 03:39:13 +0200

    Add `textmate-glsl`.

---
+++ Casks/textmate-glsl.rb
@@ -0,0 +1,16 @@
+cask 'textmate-glsl' do
+  version :latest
+  sha256 :no_check
+
+  url "https://github.com/reitermarkus/GLSL.tmbundle/archive/master.tar.gz"
+  name 'EditorConfig Plugin for TextMate'
+  homepage 'https://github.com/reitermarkus/GLSL.tmbundle'
+
+  plugin_name = 'GLSL.tmbundle'
+
+  artifact plugin_name, target: "#{ENV['HOME']}/Library/Application Support/TextMate/Bundles/#{plugin_name}"
+
+  preflight do
+    FileUtils.mv staged_path/"#{plugin_name}-master", staged_path/plugin_name
+  end
+end
