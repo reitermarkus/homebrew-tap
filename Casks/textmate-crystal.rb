eb13053c4d03b46e16890317266d5f19db740776
Markus Reiter <me@reitermark.us>
2017-05-16 21:28:58 +0200

    Add `textmate-crystal`.

---
+++ Casks/textmate-crystal.rb
@@ -0,0 +1,16 @@
+cask 'textmate-crystal' do
+  version :latest
+  sha256 :no_check
+
+  url "https://github.com/huacnlee/Crystal.tmbundle/archive/master.tar.gz"
+  name 'Crystal Plugin for TextMate'
+  homepage 'https://github.com/huacnlee/Crystal.tmbundle'
+
+  plugin_name = 'Crystal.tmbundle'
+
+  artifact plugin_name, target: "#{ENV['HOME']}/Library/Application Support/TextMate/Bundles/#{plugin_name}"
+
+  preflight do
+    FileUtils.mv staged_path/"#{plugin_name}-master", staged_path/plugin_name
+  end
+end
