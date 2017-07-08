f3d4de347e0eba07e98a1b2ffc58bf34831fda5a
Markus Reiter <me@reitermark.us>
2017-07-08 19:01:29 +0200

    Add `textmate-elixir`.

---
+++ Casks/textmate-elixir.rb
@@ -0,0 +1,11 @@
+cask 'textmate-elixir' do
+  version :latest
+  sha256 :no_check
+
+  url 'https://github.com/elixir-lang/elixir-tmbundle/archive/master.tar.gz'
+  name 'Crystal Plugin for TextMate'
+  homepage 'https://github.com/elixir-lang/elixir-tmbundle'
+
+  artifact 'elixir-tmbundle-master',
+           target: "#{ENV['HOME']}/Library/Application Support/TextMate/Bundles/Elixir.tmbundle"
+end
