a15c14592b641cfb9eb14f18fcbd598810d750da
Markus Reiter <me@reitermark.us>
2017-11-09 21:55:50 +0100

    Add `textmate-openhab`.

---
+++ Casks/textmate-openhab.rb
@@ -0,0 +1,13 @@
+cask 'textmate-openhab' do
+  version :latest
+  sha256 :no_check
+
+  url 'https://github.com/reitermarkus/OpenHAB.tmbundle/archive/master.tar.gz'
+  name 'OpenHAB for TextMate'
+  homepage 'https://github.com/reitermarkus/OpenHAB.tmbundle'
+
+  plugin_name = 'OpenHAB.tmbundle'
+
+  artifact "#{plugin_name}-master",
+           target: "#{ENV['HOME']}/Library/Application Support/TextMate/Bundles/#{plugin_name}"
+end
