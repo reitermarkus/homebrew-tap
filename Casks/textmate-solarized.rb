49eea507001430c507da5cc31f637a8743bb66e4
Markus Reiter <me@reitermark.us>
2018-06-17 01:27:49 +0200

    Add `textmate-solarized`.

---
+++ Casks/textmate-solarized.rb
@@ -0,0 +1,16 @@
+cask 'textmate-solarized' do
+  version :latest
+  sha256 :no_check
+
+  url 'https://github.com/reitermarkus/Solarized.tmbundle/archive/master.tar.gz'
+  name 'Solarized Theme for TextMate'
+  homepage 'https://github.com/reitermarkus/Solarized.tmbundle'
+
+  plugin_name = 'Solarized.tmbundle'
+
+  artifact plugin_name, target: "#{ENV['HOME']}/Library/Application Support/TextMate/Bundles/#{plugin_name}"
+
+  preflight do
+    FileUtils.mv staged_path / "#{plugin_name}-master", staged_path / plugin_name
+  end
+end
