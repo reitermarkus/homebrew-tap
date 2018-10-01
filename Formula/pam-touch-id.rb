93536e67b4f04ee45d3154abef1d0caf160b983f
Markus Reiter <me@reitermark.us>
2018-10-01 13:38:41 +0200

    Switch to `Reflejo/pam-touchID`.

--- Formula/pam-touch-id.rb
+++ Formula/pam-touch-id.rb
@@ -1,21 +1,22 @@
 class PamTouchId < Formula
   desc "PAM module for TouchID"
-  homepage "https://github.com/hamzasood/pam_touchid"
-  url "https://github.com/hamzasood/pam_touchid/archive/ec7b7bdc1285b3588fe083d5bb9ac5ab5137fda2.tar.gz"
-  version "1"
-  sha256 "5e8de62996bc2b72a407f241ed9606acd917337e431731e7714995f01fa31723"
+  homepage "https://github.com/reitermarkus/pam-touchID"
+  url "https://github.com/reitermarkus/pam-touchID/archive/daacf80cff3e600c9803aa00aa0f9ff1ec5303b8.tar.gz"
+  version "2"
+  sha256 "704e9bee7dbd91e2416a3cd0169f72e1d610e2388f889a993b36e95e4b2d67d5"
 
   keg_only "it needs to be installed manually as root"
 
   def install
-    xcodebuild "-project", "pam_touchid.xcodeproj", "SYMROOT=build", "-sdk", MacOS.sdk_path
+    system "make"
 
     (bin/"pam_touchid_install").write <<~SH
       #!/bin/sh
+      "#{bin/"pam_touchid_uninstall"}"
       mkdir -p /usr/local/lib/pam
-      cp -f #{lib}/pam/pam_touchid.so.2 /usr/local/lib/pam/pam_touchid.so.2
-      sudo chmod 444 /usr/local/lib/pam/pam_touchid.so.2
-      sudo chown root:wheel /usr/local/lib/pam/pam_touchid.so.2
+      cp -f #{lib}/pam/pam_touchid.so /usr/local/lib/pam/pam_touchid.so.#{version}
+      sudo chmod 444 /usr/local/lib/pam/pam_touchid.so.#{version}
+      sudo chown root:wheel /usr/local/lib/pam/pam_touchid.so.#{version}
       sudo sed -i '' '2 i\\
                       auth       sufficient     pam_touchid.so          reason="execute a command as another user"
                      ' /etc/pam.d/sudo
@@ -24,13 +25,11 @@ class PamTouchId < Formula
     (bin/"pam_touchid_uninstall").write <<~SH
       #!/bin/sh
       sudo sed -i '' -E '/^auth +sufficient +pam_touchid.so */d' /etc/pam.d/sudo
-      sudo rm -f /usr/local/lib/pam/pam_touchid.so.2
+      sudo rm -f /usr/local/lib/pam/pam_touchid.so.#{version}
       rmdir /usr/local/lib/pam 2>/dev/null
     SH
 
-    chmod "+x", Dir["bin/*"]
-
-    (lib/"pam").install "build/Release/pam_touchid.so.2"
+    (lib/"pam").install "pam_touchid.so"
   end
 
   def caveats
