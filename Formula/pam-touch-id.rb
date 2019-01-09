88de84adcd5bb9fe60e48a7dbe1063d5a4c7305b
Markus Reiter <me@reitermark.us>
2019-01-09 18:24:06 +0100

    Update `pam-touch-id`.

--- Formula/pam-touch-id.rb
+++ Formula/pam-touch-id.rb
@@ -1,9 +1,9 @@
 class PamTouchId < Formula
   desc "PAM module for TouchID"
   homepage "https://github.com/reitermarkus/pam-touchID"
-  url "https://github.com/reitermarkus/pam-touchID/archive/daacf80cff3e600c9803aa00aa0f9ff1ec5303b8.tar.gz"
+  url "https://github.com/reitermarkus/pam-touchID/archive/7eed8df1ee73f6adf3f9079c65a29a16fe4d22f4.tar.gz"
   version "2"
-  sha256 "704e9bee7dbd91e2416a3cd0169f72e1d610e2388f889a993b36e95e4b2d67d5"
+  sha256 "6eae7c00b0fd96a9b60f96454b1d8dabc2de93484a9b6945c3842b29c94350cd"
 
   keg_only "it needs to be installed manually as root"
 
@@ -14,18 +14,18 @@ class PamTouchId < Formula
       #!/bin/sh
       "#{bin/"pam_touchid_uninstall"}"
       mkdir -p /usr/local/lib/pam
-      cp -f #{lib}/pam/pam_touchid.so /usr/local/lib/pam/pam_touchid.so.#{version}
-      sudo chmod 444 /usr/local/lib/pam/pam_touchid.so.#{version}
-      sudo chown root:wheel /usr/local/lib/pam/pam_touchid.so.#{version}
+      cp -f #{lib}/pam/pam_touchid.so /usr/local/lib/pam/pam_touchid.so.2
+      sudo chmod 444 /usr/local/lib/pam/pam_touchid.so.2
+      sudo chown root:wheel /usr/local/lib/pam/pam_touchid.so.2
       sudo sed -i '' '2 i\\
-                      auth       sufficient     pam_touchid.so          reason="execute a command as another user"
+                      auth       sufficient     pam_touchid.so          "reason=execute a command as another user"
                      ' /etc/pam.d/sudo
     SH
 
     (bin/"pam_touchid_uninstall").write <<~SH
       #!/bin/sh
       sudo sed -i '' -E '/^auth +sufficient +pam_touchid.so */d' /etc/pam.d/sudo
-      sudo rm -f /usr/local/lib/pam/pam_touchid.so.#{version}
+      sudo rm -f /usr/local/lib/pam/pam_touchid.so.2
       rmdir /usr/local/lib/pam 2>/dev/null
     SH
 
