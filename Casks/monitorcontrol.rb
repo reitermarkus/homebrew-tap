c496f18a2d00ffee5c8941528a75c5415e5b9646
Markus Reiter <me@reitermark.us>
2019-04-13 21:23:44 +0200

    Update `monitorcontrol`.

--- Casks/monitorcontrol.rb
+++ Casks/monitorcontrol.rb
@@ -1,6 +1,6 @@
 cask 'monitorcontrol' do
-  version '1.7.0'
-  sha256 '47876c98577202cb96fdcd1a9b88897a770ca9af93f904b35d94b087d379d492'
+  version '1.8.0'
+  sha256 '6eab7860dffcccd8f66acda74b3be8cffa64599a5db7ec2350ee1be310237e6a'
 
   url "https://github.com/reitermarkus/MonitorControl/releases/download/#{version}/MonitorControl-#{version}.zip"
   appcast 'https://github.com/reitermarkus/MonitorControl/releases.atom'
