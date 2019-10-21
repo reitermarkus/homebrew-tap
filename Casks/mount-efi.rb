1f48ca35a853bd4b74877b3940bd999ace2c1e43
Markus Reiter <me@reitermark.us>
2019-10-22 01:52:30 +0200

    Add Mount EFI

---
+++ Casks/mount-efi.rb
@@ -0,0 +1,10 @@
+cask 'mount-efi' do
+  version :latest
+  sha256 :no_check
+
+  url 'https://github.com/corpnewt/MountEFI/raw/update/Mount%20EFI%20Automator%20Quick%20Action.zip'
+  name 'Mount EFI'
+  homepage 'https://github.com/corpnewt/MountEFI'
+
+  service 'Mount EFI.workflow'
+end
