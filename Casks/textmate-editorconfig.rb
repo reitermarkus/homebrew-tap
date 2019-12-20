34ada6ad193aa6bb15d6bbecf43dcdd11883b9d9
Claudia <claui@users.noreply.github.com>
2019-12-20 13:27:33 +0100

    Update `textmate-editorconfig`.

--- Casks/textmate-editorconfig.rb
+++ Casks/textmate-editorconfig.rb
@@ -1,8 +1,8 @@
 cask 'textmate-editorconfig' do
-  version '0.2.5'
-  sha256 'b046a33772396277915618f3d821445ecccafe269f82ee1b2adefcdb9088115d'
+  version '0.4.0'
+  sha256 'fd6b4aca1d0b044dcd366f0459f3da0d82a793ed2b9e956cd6b969b1f42ed451'
 
-  url "https://github.com/Mr0grog/editorconfig-textmate/releases/download/v#{version}/editorconfig-textmate-#{version}.tar.gz"
+  url "https://github.com/Mr0grog/editorconfig-textmate/releases/download/v#{version}/editorconfig-textmate-#{version}.tmplugin.zip"
   appcast 'https://github.com/Mr0grog/editorconfig-textmate/releases.atom',
           checkpoint: '383f3712d5a3fe4516fcc79350c7572ae00adc28530683993ec7d36e6556206c'
   name 'EditorConfig Plugin for TextMate'
