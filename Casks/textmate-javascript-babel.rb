a7807f53a90ff363262e9753cf3c86022592c823
Markus Reiter <me@reitermark.us>
2020-08-16 22:00:10 +0200

    Fix branch for `textmate-javascript-babel`.

--- Casks/textmate-javascript-babel.rb
+++ Casks/textmate-javascript-babel.rb
@@ -2,10 +2,10 @@ cask "textmate-javascript-babel" do
   version :latest
   sha256 :no_check
 
-  url "https://github.com/ravasthi/javascript-babel.tmbundle/archive/master.tar.gz"
+  url "https://github.com/ravasthi/javascript-babel.tmbundle/archive/main.tar.gz"
   name "JavaScript (Babel) Plugin for TextMate"
   homepage "https://github.com/ravasthi/javascript-babel.tmbundle"
 
-  artifact "javascript-babel.tmbundle-master",
+  artifact "javascript-babel.tmbundle-main",
            target: "#{ENV["HOME"]}/Library/Application Support/TextMate/Bundles/JavaScript (Babel).tmbundle"
 end
