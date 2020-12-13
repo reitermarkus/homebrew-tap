efebc10bde1b15564199b398c700409026f088eb
Markus Reiter <me@reitermark.us>
2020-12-13 01:45:10 +0100

    Update `textmate-solarized`.

--- Casks/textmate-solarized.rb
+++ Casks/textmate-solarized.rb
@@ -2,15 +2,17 @@ cask "textmate-solarized" do
   version :latest
   sha256 :no_check
 
-  url "https://github.com/reitermarkus/Solarized.tmbundle/archive/master.tar.gz"
-  name "Solarized Theme for TextMate"
+  url "https://github.com/reitermarkus/Solarized.tmbundle/tarball/HEAD"
+  name "TextMate Solarized Theme"
+  desc "Text editor color theme"
   homepage "https://github.com/reitermarkus/Solarized.tmbundle"
 
   plugin_name = "Solarized.tmbundle"
 
-  artifact plugin_name, target: "#{ENV["HOME"]}/Library/Application Support/TextMate/Bundles/#{plugin_name}"
+  artifact plugin_name, target: "~/Library/Application Support/TextMate/Bundles/#{plugin_name}"
 
   preflight do
-    FileUtils.mv staged_path / "#{plugin_name}-master", staged_path / plugin_name
+    plugin_path = staged_path.children.first
+    FileUtils.mv plugin_path, staged_path / plugin_name
   end
 end
