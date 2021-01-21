f02e9ec1165eb649262d0867d5163802f4c53ac2
Markus Reiter <me@reitermark.us>
2021-01-21 20:38:33 +0100

    Update ssd-fan-control.rb

--- Casks/ssd-fan-control.rb
+++ Casks/ssd-fan-control.rb
@@ -2,9 +2,9 @@ cask "ssd-fan-control" do
   version "2.13"
   sha256 "38bb039750a81300b732b2e17b3d9fa033b621272d2c50b5322528509f4adc6f"
 
-  url "http://exirion.net/software/SSDFanControl-#{version}.dmg"
+  url "https://exirion.net/software/SSDFanControl-#{version}.dmg"
   name "SSD Fan Control"
-  homepage "http://exirion.net/ssdfanctrl/"
+  homepage "https://exirion.net/ssdfanctrl/"
 
   livecheck do
     url :homepage
