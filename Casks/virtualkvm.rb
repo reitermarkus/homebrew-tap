7aaacb9ee031ea07476332e27c1444923eaaa0d3
Markus Reiter <me@reitermark.us>
2021-02-12 22:57:00 +0100

    Update virtualkvm.rb

--- Casks/virtualkvm.rb
+++ Casks/virtualkvm.rb
@@ -2,7 +2,7 @@ cask "virtualkvm" do
   version "1.2.4"
   sha256 "6e4fccd8640838b7ef372205d6fe812c64ef185b7f53265588cd18471c4c1f0a"
 
-  url "https://github.com/alvaromurillo/VirtualKVM/releases/download/#{version}/VirtualKVM.zip"
+  url "https://github.com/duanefields/VirtualKVM/releases/download/v#{version}/v#{version.dots_to_underscores}.zip"
   name "VirtualKVM"
   homepage "https://github.com/duanefields/VirtualKVM"
 
