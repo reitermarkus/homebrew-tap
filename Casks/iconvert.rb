751488e69f4d011b224137390c4dbcbe14a91c35
Markus Reiter <me@reitermark.us>
2022-03-08 18:43:59 +0100

    Fix style.

--- Casks/iconvert.rb
+++ Casks/iconvert.rb
@@ -1,8 +1,8 @@
 cask "iconvert" do
   version "1.0.0"
-  sha256 "b598453a1f0ae9a8ed36930f9ceee0af209f0833850db41403ae1572ce9dc51f"
+  sha256 :no_check
 
-  url "https://github.com/reitermarkus/mirror/raw/master/iConvert.zip"
+  url "https://github.com/reitermarkus/mirror/raw/HEAD/iConvert.zip"
   name "iConvert"
   homepage "http://www.rmartijnr.eu/iconvert.html"
 
