129fbd5c1846bd0ce1d593fc1b7879063553dbcc
Markus Reiter <me@reitermark.us>
2022-03-08 18:43:14 +0100

    Fix style.

--- Casks/itest.rb
+++ Casks/itest.rb
@@ -1,8 +1,8 @@
 cask "itest" do
   version "1.1.0"
-  sha256 "4855b94b42b7c6bc9e0c121a75706a5b94199c393017e8a08ec1ceb5a377bc1e"
+  sha256 :no_check
 
-  url "https://github.com/reitermarkus/mirror/raw/master/iTest.zip"
+  url "https://github.com/reitermarkus/mirror/raw/HEAD/iTest.zip"
   name "iTest"
   homepage "http://www.rmartijnr.eu/iTest/index.html"
 
