d6d2187604a83fc09f4ba96cbda91c7da4a952b2
Markus Reiter <me@reitermark.us>
2022-03-08 18:44:21 +0100

    Fix style.

--- Casks/ilearn.rb
+++ Casks/ilearn.rb
@@ -1,8 +1,8 @@
 cask "ilearn" do
   version "1.3.3"
-  sha256 "06b8db063df075efda1b29384333d25f5b8aaf809dea476576a030f13003c811"
+  sha256 :no_check
 
-  url "https://github.com/reitermarkus/mirror/raw/master/iLearn.zip"
+  url "https://github.com/reitermarkus/mirror/raw/HEAD/iLearn.zip"
   name "iLearn"
   homepage "http://www.rmartijnr.eu/iLearn/index.html"
 
