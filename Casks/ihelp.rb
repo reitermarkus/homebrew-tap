fcbbea740644e41a55f73d5906d487c763e98db6
Markus Reiter <me@reitermark.us>
2022-03-08 19:33:45 +0100

    Add `verified` parameter.

--- Casks/ihelp.rb
+++ Casks/ihelp.rb
@@ -2,7 +2,8 @@ cask "ihelp" do
   version "2.0.1"
   sha256 :no_check
 
-  url "https://github.com/reitermarkus/mirror/raw/HEAD/iHelp.zip"
+  url "https://github.com/reitermarkus/mirror/raw/HEAD/iHelp.zip",
+    verified: "https://github.com/reitermarkus/mirror/"
   name "iHelp"
   homepage "http://www.rmartijnr.eu/iHelp/index.html"
 
