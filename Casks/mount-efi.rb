769019c59350222045114878b23769f1988ec765
Markus Reiter <me@reitermark.us>
2022-03-08 19:06:32 +0100

    Update Mount EFI.

--- Casks/mount-efi.rb
+++ Casks/mount-efi.rb
@@ -2,7 +2,7 @@ cask "mount-efi" do
   version :latest
   sha256 :no_check
 
-  url "https://github.com/corpnewt/MountEFI/raw/update/Mount%20EFI%20Automator%20Quick%20Action.zip"
+  url "https://github.com/corpnewt/MountEFI/raw/update/Mount%20EFI%20Automator%20Quick%20Action%20Python%203.zip"
   name "Mount EFI"
   homepage "https://github.com/corpnewt/MountEFI"
 
