b16dc5add69493c9ef8c23008836b26f3efb012d
Markus Reiter <me@reitermark.us>
2022-03-13 08:23:06 +0100

    Update SheepShaver version interpolation.

--- Casks/sheepshaver.rb
+++ Casks/sheepshaver.rb
@@ -2,9 +2,9 @@ cask "sheepshaver" do
   version "2.4.20140201"
   sha256 "545bcd5348c5ce88d18f3f0f01971107472208d1c84361d94c8ce2c238212788"
 
-  url "http://www.xs4all.nl/~ronaldpr/sheepshaverforum/SheepShaver_UB_20140201.zip"
+  url "http://www.xs4all.nl/~ronaldpr/sheepshaverforum/SheepShaver_UB_#{version.patch}.zip"
   name "SheepShaver"
   homepage "http://sheepshaver.cebix.net/"
 
-  app "SheepShaver_UB_20140201/SheepShaver.app"
+  app "SheepShaver_UB_#{version.patch}/SheepShaver.app"
 end
