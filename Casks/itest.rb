dd5add22f2451419675a8c1d294d682ea8d9485b
Markus Reiter <me@reitermark.us>
2023-03-11 06:23:18 +0100

    Update itest from 1.1.0 to 1.1.1

--- Casks/itest.rb
+++ Casks/itest.rb
@@ -1,9 +1,9 @@
 cask "itest" do
-  version "1.1.0"
+  version "1.1.1"
   sha256 :no_check
 
   url "https://github.com/reitermarkus/mirror/raw/HEAD/iTest.zip",
-      verified: "https://github.com/reitermarkus/mirror/"
+      verified: "github.com/reitermarkus/mirror/"
   name "iTest"
   homepage "http://www.rmartijnr.eu/iTest/index.html"
 
