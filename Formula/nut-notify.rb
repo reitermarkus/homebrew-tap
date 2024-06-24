060de07c9cf527809f2d844251ce57cfceed5fcf
Markus Reiter <me@reitermark.us>
2024-06-24 19:29:32 +0200

    Fix `nut-notify`.

--- Formula/nut-notify.rb
+++ Formula/nut-notify.rb
@@ -3,7 +3,7 @@ class NutNotify < Formula
   homepage "https://github.com/reitermarkus/homebrew-tap"
   url "https://example.org/index.html"
   version "1.0.0"
-  sha256 :no_check
+  sha256 "ea8fac7c65fb589b0d53560f5251f74f9e9b243478dcb6b3ea79b5e36449c8d9"
 
   depends_on "reattach-to-user-namespace"
   depends_on "terminal-notifier"
@@ -23,16 +23,19 @@ class NutNotify < Formula
       case "${NOTIFYTYPE}" in
         ONLINE|COMMOK)
           APP_ICON='/System/Library/CoreServices/CoreTypes.bundle/Contents/Resources/GenericNetworkIcon.icns'
+          SENDER='com.apple.AVB-Audio-Configuration'
           ;;
         SHUTDOWN|FSD)
           APP_ICON='/System/Library/CoreServices/CoreTypes.bundle/Contents/Resources/AlertStopIcon.icns'
+          SENDER='com.apple.controlcenter'
           ;;
         *)
           APP_ICON='/System/Library/CoreServices/CoreTypes.bundle/Contents/Resources/AlertCautionIcon.icns'
+          SENDER='com.apple.ActivityMonitor'
           ;;
       esac
 
-      su "${CURRENT_USER}" -c "'#{Formula["reattach-to-user-namespace"].opt_bin}/reattach-to-user-namespace' '#{Formula["terminal-notifier"].opt_bin}/terminal-notifier' -ignoreDnD -appIcon '${APP_ICON}' -sender com.apple.ActivityMonitor -title 'UPS' -message '${@}'"
+      su "${CURRENT_USER}" -c "'#{Formula["reattach-to-user-namespace"].opt_bin}/reattach-to-user-namespace' '#{Formula["terminal-notifier"].opt_bin}/terminal-notifier' -ignoreDnD -appIcon '${APP_ICON}' -sender '${SENDER}' -title 'UPS' -message '${@}'"
     EOS
 
     chmod "+x", prefix/"etc/nut/notify.sh"
