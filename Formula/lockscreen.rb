18e690e1b3e8d3e5396305163221d9754b9bbe67
Markus Reiter <me@reitermark.us>
2024-06-26 21:18:15 +0200

    Update CI config.

--- Formula/lockscreen.rb
+++ Formula/lockscreen.rb
@@ -1,7 +1,7 @@
 class Lockscreen < Formula
   desc "Utility to lock your screen"
   homepage "https://github.com/reitermarkus/lockscreen"
-  url "https://github.com/reitermarkus/lockscreen/archive/1.0.0.tar.gz"
+  url "https://github.com/reitermarkus/lockscreen/archive/refs/tags/1.0.0.tar.gz"
   sha256 "918e57b662f764ae5a5c98c9b37537ac97471cf46bd30910c71aa67281c925ff"
 
   def install
--- Formula/rbenv-system-ruby.rb
+++ Formula/rbenv-system-ruby.rb
@@ -1,7 +1,7 @@
 class RbenvSystemRuby < Formula
   desc "Plugin to make rbenv compatible with the system Ruby"
   homepage "https://github.com/reitermarkus/rbenv-system-ruby"
-  url "https://github.com/reitermarkus/rbenv-system-ruby/archive/1.0.4.tar.gz"
+  url "https://github.com/reitermarkus/rbenv-system-ruby/archive/refs/tags/1.0.4.tar.gz"
   sha256 "0d06ee1ad33b988cba2d27511ab487940bf0c1bf968cd6fd0bffc1b054ca3c01"
   head "https://github.com/reitermarkus/rbenv-system-ruby.git"
 
